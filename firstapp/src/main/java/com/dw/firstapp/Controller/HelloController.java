44866d9cbad9468e2b6d619b9493806e1da4002e
RYU185 <fence365@naver.com>
2024-12-11 15:23:34 +0900

    Update HelloController.java

--- firstapp/src/main/java/com/dw/firstapp/Controller/HelloController.java
+++ firstapp/src/main/java/com/dw/firstapp/Controller/HelloController.java
@@ -3,7 +3,7 @@ package com.dw.firstapp.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController  // 이 어노테이션이 아래 클래스에서 /sayhello를 입력했을 때 메서드를 수행하게 해줌
 public class HelloController {
     @GetMapping("/sayhello")  // 대부분 문자열이 들어가고 슬래쉬(/) 시작 후 소문자
         public String hello() {
