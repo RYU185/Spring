8e6f34cb12e47ebd1f0d2dbed3a8d5bb42a9692d
RYU185 <fence365@naver.com>
2024-12-30 11:43:22 +0900

    12/30 add

---
+++ jpaapp/src/main/java/com/dw/jpaapp/model/InstructorProfile.java
@@ -0,0 +1,42 @@
+package com.dw.jpaapp.model;
+
+import com.dw.jpaapp.DTO.InstructorDTO;
+import com.dw.jpaapp.DTO.InstructorProfileDTO;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name = "instructor_profile")
+@Entity
+public class InstructorProfile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "bio", length = 3000 ) // length는 글자수 (바이트 X)
+    private String bio; // 강사 소개
+
+    @Column(name = "github_url")
+    private String githubUrl;
+
+    @OneToOne
+    @JoinColumn(name = "instructor_id") // 단방향 참조
+    private Instructor instructor;
+
+    // Instructor 매핑 메서드
+    public InstructorProfileDTO toDTO(){
+        return new InstructorProfileDTO(
+                this.id,
+                this.bio,
+                this.githubUrl,
+                this.instructor.getId());
+    }
+}
