2cf42c28db57126ef64c2a4c2c5bdfc1f68e61aa
RYU185 <fence365@naver.com>
2025-01-16 01:04:14 +0900

    .

--- dynamic/src/main/java/com/dw/dynamic/repository/UserRepository.java
+++ dynamic/src/main/java/com/dw/dynamic/repository/UserRepository.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User,String> {
+
     List<User> findByRealNameLike(String realName);
 
     List<User> findUserByExistBusinessOperator(boolean existBusinessOperator);
