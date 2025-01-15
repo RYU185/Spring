6f9614e426eb87b63aecfd2b1bb3aa3d2c23a1c3
RYU185 <fence365@naver.com>
2025-01-15 14:25:17 +0900

    .

--- dynamic/src/main/java/com/dw/dynamic/service/UserService.java
+++ dynamic/src/main/java/com/dw/dynamic/service/UserService.java
@@ -1,7 +1,40 @@
 package com.dw.dynamic.service;
 
+import com.dw.dynamic.DTO.UserDTO;
+import com.dw.dynamic.model.User;
+import com.dw.dynamic.repository.AuthorityRepository;
+import com.dw.dynamic.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Service
 public class UserService {
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    BCryptPasswordEncoder passwordEncoder;
+    @Autowired
+    AuthorityRepository authorityRepository;
+
+    public UserDTO registerUser(UserDTO userDTO) {
+        Optional<User> user = userRepository.findById(userDTO.getUserName());
+        if (user.isPresent()) {
+            throw new InvalidRequestException("Username already exists");
+        }
+        return userRepository.save(
+                new User(
+                        userDTO.getUserName(),
+                        passwordEncoder.encode(userDTO.getPassword()),
+                        userDTO.getEmail(),
+                        userDTO.getRealName(),
+                        authorityRepository.findById("USER")
+                                .orElseThrow(()->new ResourceNotFoundException("No role")),
+                        LocalDateTime.now())
+        ).toDto();
+    }
+
 }
