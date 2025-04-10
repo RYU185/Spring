65d88cf53f536ba38a2f7fb46fa2aaf18cea62ad
RYU185 <fence365@naver.com>
2025-04-10 11:40:24 +0900

    123

--- gameshop_jwt/src/main/java/dw/gameshop/chat/ChatController.java
+++ gameshop_jwt/src/main/java/dw/gameshop/chat/ChatController.java
@@ -6,26 +6,26 @@ import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
 
-@Controller
-public class ChatController {
+    @Controller
+    public class ChatController {
 
-    @MessageMapping("/chat.sendMessage")
-    @SendTo("/topic/public") // 구독명이 SendTo
-    public ChatMessage sendMessage(
-            @Payload ChatMessage chatMessage
-    ) {
-        return chatMessage;
-    }
+        @MessageMapping("/chat.sendMessage")
+        @SendTo("/topic/public") // 구독명이 SendTo
+        public ChatMessage sendMessage(
+                @Payload ChatMessage chatMessage
+        ) {
+            return chatMessage;
+        }
 
-    @MessageMapping("/chat.addUser") // 입장
-    @SendTo("/topic/public")
-    public ChatMessage addUser(
-            @Payload ChatMessage chatMessage,
-            SimpMessageHeaderAccessor headerAccessor
-    ) {
-        // Add username in web socket session
-        // 연결종료 시 누구의 연결이 종료된 것인지 확인하기 위해 저장
-        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
-        return chatMessage;
+        @MessageMapping("/chat.addUser") // 입장
+        @SendTo("/topic/public")
+        public ChatMessage addUser(
+                @Payload ChatMessage chatMessage,
+                SimpMessageHeaderAccessor headerAccessor
+        ) {
+            // Add username in web socket session
+            // 연결종료 시 누구의 연결이 종료된 것인지 확인하기 위해 저장
+            headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+            return chatMessage;
+        }
     }
-}
